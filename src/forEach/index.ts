88e96af1213ae03a53251f65c613a2e4969ace41
Benjamín Guzmán <benjamingl99@gmail.com>
2024-11-07 17:06:43 -0300

    enhance documentation for forEach function with additional remarks and type template

--- src/forEach/index.ts
+++ src/forEach/index.ts
@@ -2,8 +2,15 @@ import { cooperativeFor } from "../for";
 
 /**
  * Calls a defined callback function on each element of an array.
+ *
+ * @remarks
+ * Calls a defined callback function on each element of an array. The forEach method calls the callbackfn function one time for each element in the array.
+ *
+ * @template T - The type of the elements in the array.
+ *
  * @param arrayOrPromise - The array or Promise of an array to iterate over.
  * @param callbackfn - A function that accepts up to three arguments. The forEach method calls the callbackfn function one time for each element in the array.
+ *
  * @returns A Promise that resolves when the loop is finished.
  *
  * @example
